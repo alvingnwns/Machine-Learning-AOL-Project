cd391ac90db1a76fc6a18d296405219ea49eeecc
alvingnwns <leonardus.alving@outlook.com>
2026-05-03 14:43:25 +0700

    Update: Updated the dataset, EDA notebook, and train_model

--- backend/src/train_model.py
+++ backend/src/train_model.py
@@ -6,7 +6,7 @@ import os
 def train_and_save_knn():
     print("Memulai proses training model KNN...")
 
-    data_path = '../data/cafe_orders_dataset.csv'
+    data_path = '../data/bread basket.csv'
     output_path = '../models/knn_recommendations.json'
 
     try:
@@ -16,20 +16,24 @@ def train_and_save_knn():
         print(f"Error: File tidak ditemukan pada {data_path}")
         return
     
-    print("Membangun matriks interaksi antar item dengan user...")
-    item_user_matrix = pd.crosstab(df['CoffeeType'], df['CustomerName']).clip(upper=1)
+    df.columns = df.columns.str.strip()
+
+    if 'Item' in df.columns:
+        df = df[df['Item'] != 'NONE']
+
+    print("Membangun matriks interaksi antar item-transaction...")
     item_transaction_matrix = pd.crosstab(df['Item'], df['Transaction']).clip(upper=1)
 
     print("Melatih model KNN...")
     knn = NearestNeighbors(metric='cosine', algorithm='brute', n_neighbors=5)
-    knn.fit(item_user_matrix.values)
+    knn.fit(item_transaction_matrix.values)
 
     rekomendasi_menu = {}
 
-    for i, item in enumerate(item_user_matrix.index):
-        distances, indices = knn.kneighbors(item_user_matrix.iloc[i, :].values.reshape(1, -1))
+    for i, item in enumerate(item_transaction_matrix.index):
+        distances, indices = knn.kneighbors(item_transaction_matrix.iloc[i, :].values.reshape(1, -1))
 
-        rekomendasi = [item_user_matrix.index[idx] for idx in indices.flatten()[1:]]
+        rekomendasi = [item_transaction_matrix.index[idx] for idx in indices.flatten()[1:]]
         rekomendasi_menu[item] = rekomendasi
     
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
